f26cb00833dac03baf5f19a962296318cdbde04b
Debashis <dmohanty962@gmail.com>
2020-12-06 09:15:37 +0530

    static_files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    blog_obj=Blog.objects.all()
+    return render(request, 'blog/all_blogs.html',{'bo':blog_obj})
