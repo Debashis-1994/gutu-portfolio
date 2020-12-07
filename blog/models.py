c5abcc8756b54a476483b3649b261ae05d7ca39d
Debashis <dmohanty962@gmail.com>
2020-12-07 22:19:51 +0530

    renew 1.0

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField(max_length=450)
     date=models.DateField()
+
+    def __str__(self):
+        return self.title
